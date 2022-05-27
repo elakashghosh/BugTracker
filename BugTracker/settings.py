6c8af84a5b14972e461dddedea61e0c1fd0a3a3a
Akash <akashghosh980@gmail.com>
2022-05-27 21:19:04 +0530

    GFG

--- BugTracker/settings.py
+++ BugTracker/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import dj_database_url
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-_j04wtrdao22oq&t02nav(wawm+^8qas#df(0-+$)4sd5o&ux9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
